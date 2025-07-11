f42f8c95f88021e53037eeacc1184c7946c8473b
Yang Renyi <jianghurainxwy@gmail.com>
2025-07-11 17:30:06 +0800

    jsonl

--- lhotse_util.py
+++ lhotse_util.py
@@ -1,7 +1,7 @@
 from lhotse import CutSet
 from lhotse.recipes import prepare_librispeech
 from lhotse.cut import append_cuts
-
+import json
 
 def from_strategy_to_cuts(source_cuts, strategy: list):
     """
@@ -17,4 +17,23 @@ def from_strategy_to_cuts(source_cuts, strategy: list):
         target_cuts_list.append(new_cut)
     return CutSet(target_cuts_list)
 
+def jsonl_head(jsonl_path, n=10):
+    """
+    Read the first n lines of a jsonl file.
+    :param jsonl_path: path to the jsonl file
+    :param n: number of lines to read
+    :return: list of dictionaries
+    """
+    assert jsonl_path.endswith('.jsonl')
+    output_path = jsonl_path[: -6] + "_head.jsonl"
+    with open(jsonl_path, 'r', encoding='utf-8') as f:
+        lines = [json.loads(line) for line in f.readlines()[:n]]
+    with open(output_path, 'w', encoding='utf-8') as f:
+        for line in lines:
+            f.write(json.dumps(line, ensure_ascii=False) + '\n')
+    return output_path
+
+if __name__ == '__main__':
+    json_path = "../datasets/LongSpeech/raw_cuts.jsonl"
+    print(jsonl_head(json_path, 10))
 
--- util.py
+++ util.py
@@ -5,8 +5,6 @@ def restore_punctuation(text: str) -> str:
     """
     标点恢复
     """
-
-
     punctuated_text = punct.restore_punctuation(text.lower())
 
 
@@ -15,7 +13,6 @@ def restore_punctuation(text: str) -> str:
     punctuated_text = '. '.join(sentence.capitalize() for sentence in sentences)
 
     return punctuated_text
-
 
 def test_restore_punctuation():
     text  = "SO THERE CAME A STEP AND A LITTLE RUSTLING OF FEMININE DRAPERIES THE SMALL DOOR OPENED AND RACHEL ENTERED WITH HER HAND EXTENDED AND A PALE SMILE OF WELCOME WOMEN CAN HIDE THEIR PAIN BETTER THAN WE MEN AND BEAR IT BETTER TOO EXCEPT WHEN SHAME DROPS FIRE INTO THE DREADFUL CHALICE"
